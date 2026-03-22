2a2c950da107eca57f1be905a0261ddce2d00ce4
thamimkhonza-12 <thamimkhonza@gmail.com>
2026-03-22 16:21:55 +0200

    Fix middleware ellipsis error

--- config/settings.py
+++ config/settings.py
@@ -52,11 +52,13 @@ INSTALLED_APPS = [
     'api',
 ]
 
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
