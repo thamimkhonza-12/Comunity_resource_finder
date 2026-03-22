ffe960dbc522c5624b7730ec1824cbe735e2beb5
thamimkhonza-12 <thamimkhonza@gmail.com>
2026-03-22 16:34:56 +0200

    Remove ellipsis causing deploy crash

--- config/settings.py
+++ config/settings.py
@@ -162,5 +162,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    ...
+    
 ]
